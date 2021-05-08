7e39fb76e7339fdc050624c85e60f5775b319690
Oluwatobi <tobirammar@gmail.com>
2021-05-08 07:20:07 +0100

    changed roles and email api key

--- mail.js
+++ mail.js
@@ -1,34 +1,30 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const mailGun = require("nodemailer-mailgun-transport");
 
 const auth = {
-    auth: {
-        api_key:'key-12e3e5b69927fb2f1d59b035d075b126',
-        domain:'mail.69unlockers.com'
-    }
-}
+  auth: {
+    api_key: "875340469e30e11646f86472f7f94df6-2a9a428a-56d8ed8f",
+    domain: "mail.compassionatebackersltd.com",
+  },
+};
 
 const transporter = nodemailer.createTransport(mailGun(auth));
 
 const sendMail = (email, subject, text, cb) => {
+  const mailOptions = {
+    from: email,
+    to: "beatsbytbee@gmail.com",
+    subject: subject,
+    text: text,
+  };
 
-    const mailOptions = {
-        from: email,
-        to: 'tobirammar@gmail.com',
-        subject: subject,
-        text: text
-    
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
     }
-    
-    transporter.sendMail(mailOptions, function(err, data){
-        if (err) {
-            cb(err, null);
-        } else {
-            cb(null, data);
-        }
-    });
-
-
-}
+  });
+};
 
 module.exports = sendMail;
