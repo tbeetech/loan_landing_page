655665e961f88dd9cebf67718f3d6260b84a445d
Oluwatobi <tobirammar@gmail.com>
2021-06-28 10:47:19 +0100

    greater

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.set("view engine", "ejs");
 
 const server = app.listen(process.env.PORT || 80)
 
-server.keepAliveTimeout = 75000
+server.keepAliveTimeout = 400000
 
 
 // console.log("compassionate server is now running");
